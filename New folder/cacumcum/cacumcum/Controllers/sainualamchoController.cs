04f39ce37cab1ea107d2d137710be9de5b89e005
dothanhchinh2000 <77373465+dothanhchinh2000@users.noreply.github.com>
2021-06-25 21:59:15 +0700

    ddddd

---
+++ New folder/cacumcum/cacumcum/Controllers/sainualamchoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cacumcum.Controllers
+{
+    public class sainualamchoController : Controller
+    {
+        // GET: sainualamcho
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ New folder/cacumcum/cacumcum/Models/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cacumcum.Models
+{
+    public class Class1
+    {
+    }
+}
